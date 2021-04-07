aeff5ffcea6a8cbd52ef86ee63b527f82f34c278
Gaston Ezequiel Campos <camposgaston@gmail.com>
2021-04-07 16:57:37 -0300

    import lines separation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
 import { MinesweeperComponent } from './components/minesweeper/minesweeper.component';
 import { GameSetupComponent } from './pages/game-setup/game-setup.component';
 import { GameBoardComponent } from './pages/game-board/game-board.component';
