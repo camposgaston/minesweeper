0b1f5f1546d29b9177ba58714d00b318190f0059
Gaston Ezequiel Campos <camposgaston@gmail.com>
2021-04-08 14:58:38 -0300

    changed default status

--- src/app/models/square.ts
+++ src/app/models/square.ts
@@ -1,5 +1,5 @@
 export class Square {
     public value = 0;
     public showsAdj: number[][] = [];
-    public status: 'hidden' | 'visible' | 'detonated' = 'hidden';
+    public status: 'hidden'| 'hidden flagged' | 'visible' | 'detonated detonation' = 'hidden';
 }
