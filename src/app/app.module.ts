95f5289a7cd7bc123c0911448f4f3d45f8d35b57
Gaston Ezequiel Campos <camposgaston@gmail.com>
2021-04-11 01:01:14 -0300

    added reactive forms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -24,7 +25,9 @@ import { ShowTimePipe } from './pipes/show-time.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
