d32acd9e5185f15ca1835ee096ab2aba56bf1a70
Gaston Ezequiel Campos <camposgaston@gmail.com>
2021-04-11 01:02:16 -0300

    Reactive form validations created

--- src/app/pages/game-setup/game-setup.component.ts
+++ src/app/pages/game-setup/game-setup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-game-setup',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameSetupComponent implements OnInit {
 
-  constructor() { }
+
+  forma = this.fb.group({
+    namePlayer1: ['', [Validators.required, Validators.minLength(5)]],
+    namePlayer2: ['', [Validators.required, Validators.minLength(5)]],
+    rows: [8, [Validators.required, Validators.min(8), Validators.max(20)]],
+    cols: [10, [Validators.required, Validators.min(10), Validators.max(24)]],
+    mines: [10, [Validators.required, Validators.min(10), Validators.max(99)]],
+    multiplayer: [false],
+    level: ['Medium']
+  });
+
+  levels = [{ name: 'Easy', rows: 8, cols: 10, mines: 10 },
+  { name: 'Medium', rows: 14, cols: 18, mines: 40 },
+  { name: 'Hard', rows: 20, cols: 24, mines: 99 },
+  { name: 'Defined by Player' }];
+
+  constructor(private fb: FormBuilder,) {
+
+  }
 
   ngOnInit(): void {
   }
+
+  get invalidNamePlayer1(): boolean | undefined {
+    return this.forma.get('namePlayer1')?.invalid && this.forma.get('namePlayer1')?.touched;
+  }
+
+  get invalidNamePlayer2(): boolean | undefined {
+    return this.forma.get('namePlayer2')?.invalid && this.forma.get('namePlayer2')?.touched;
+  }
+
+  get invalidRows(): boolean | undefined {
+    return this.forma.get('rows')?.invalid && this.forma.get('rows')?.touched;
+  }
+
+  get invalidCols(): boolean | undefined {
+    return this.forma.get('cols')?.invalid && this.forma.get('cols')?.touched;
+  }
+
+  get invalidMines(): boolean | undefined {
+    return this.forma.get('mines')?.invalid && this.forma.get('mines')?.touched;
+  }
+
+  setup(): void {
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach(control => {
+        if (control instanceof FormGroup) {
+          Object.values(control.controls).forEach(controlInstance => controlInstance.markAsTouched());
+        }
+        control.markAsTouched();
+      });
+    }
+  }
+
+
 
 }
