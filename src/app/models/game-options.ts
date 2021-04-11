694c62f3288c18d8c4c24372ae87f9548ee11341
Gaston Ezequiel Campos <camposgaston@gmail.com>
2021-04-11 19:34:55 -0300

    new game options interface

---
+++ src/app/models/game-options.ts
@@ -0,0 +1,10 @@
+export interface IGameOptions {
+    namePlayer1: string;
+    namePlayer2: string;
+    rows: number;
+    cols: number;
+    mines: number;
+}
+
+// export class GameOptions {
+// }
